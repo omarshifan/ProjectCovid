596a156d4cb645c7def622fcf4f6d67eea41afad
omarshifan <108083376+omarshifan@users.noreply.github.com>
2023-08-16 08:25:44 +0530

    Update project covid.sql

--- project covid.sql
+++ project covid.sql
@@ -7,20 +7,22 @@ WHERE location like '%india%'
 ORDER BY 1,2
 
 -- TOTAL CASES VS POPULATION
+   
 -- Depicts percentage of population that got covid
 SELECT location, date, total_cases, population, (total_cases/population)*100 as InfectionPercentage
 FROM coviddeaths
 WHERE location like '%india%'
 ORDER BY 1,2
 
--- Highest Infection rate
+-- Highest Infection rate (Tableau viz 3)
+   
 SELECT location,  MAX(total_cases) as highest_infection_rate , population, MAX((total_cases/population))*100 as InfectionPercentage
 FROM ProjectCovid..coviddeaths
 GROUP BY location, population
 ORDER BY InfectionPercentage DESC
 
 --Highest death count (Tableau viz 2)
-
+   
 SELECT location, MAX(total_deaths) as totaldeathcount
 FROM	ProjectCovid..coviddeaths
 WHERE continent is not null
@@ -36,6 +38,7 @@ GROUP BY continent
 ORDER BY totaldeathcount DESC
 
 --GLOBAL NUMBERS (Tableau viz 1)
+   
 SELECT date, SUM(new_cases), SUM(new_deaths), (SUM(new_deaths)/SUM(new_cases))*100 as DeathPercentage
 FROM ProjectCovid..coviddeaths
 WHERE continent is not null
@@ -43,6 +46,7 @@ GROUP BY date
 ORDER BY 1,2
 
 -- TOTAL POPULATION VS VACCINATION
+   
 SELECT dea.continent, dea.location, dea.date, dea.population,CAST(vac.new_vaccinations AS bigint), SUM(CAST(vac.new_vaccinations AS bigint)) OVER (Partition by dea.location Order by dea.location, dea.date) 
 as PeopleVaccinatedCumulative
 FROM ProjectCovid..coviddeaths as dea 
